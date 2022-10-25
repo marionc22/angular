40021f76184a1d0dfb544627faf8b272ca2c5518
CORBEL Marion (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <9301738k@commun.ad.sncf.fr>
2022-10-25 10:00:01 +0200

    userprofile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  hidden: boolean = false
+  hidden: boolean = true
 
   public hide(){
    this.hidden= !this.hidden
