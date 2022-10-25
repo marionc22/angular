2a352168c9b26219c068669b99d1235687ea48f6
CORBEL Marion (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <9301738k@commun.ad.sncf.fr>
2022-10-25 09:58:57 +0200

    userprofile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  hidden: boolean = false
+
+  public hide(){
+   this.hidden= !this.hidden
+  }
+
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : 'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
